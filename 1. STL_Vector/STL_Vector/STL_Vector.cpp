6dbb7d4b79eba7dd5dd49115a7970e4f11342502
I-am-Roman <onezhnov@list.ru>
2022-06-26 10:27:42 +0300

    Add cope and move constructur

--- 1. STL_Vector/STL_Vector/STL_Vector.cpp
+++ 1. STL_Vector/STL_Vector/STL_Vector.cpp
@@ -1,4 +1,5 @@
 ﻿#include "test_runner.h"
+#include <algorithm>
 using namespace std;
 
 /****************************************************************/
@@ -12,14 +13,19 @@ private:
     size_t size;
     size_t capacity;
 public:
-    SimpleVector() = default;
-    explicit SimpleVector(size_t size);
+    SimpleVector():size(0),capacity(0) {};
+    explicit SimpleVector(size_t size) noexcept;
+    //move constructor
+    SimpleVector(SimpleVector<T>&& other);
+    //copy constructor
+    SimpleVector(const SimpleVector<T>& other);
+
     ~SimpleVector();
 
     T& operator[](size_t index);
 
-    T* begin();
-    T* end();
+    T* begin() const;
+    T* end() const;
 
     size_t Size() const;
     size_t Capacity() const;
@@ -28,9 +34,29 @@ public:
 };
 
 template <typename T>
-SimpleVector<T>::SimpleVector(size_t _size) : 
+SimpleVector<T>::SimpleVector(size_t _size) noexcept: 
     value(new T[_size]), size(_size), capacity(_size)
 {}
+
+//move constructor
+template <typename T>
+SimpleVector<T>::SimpleVector(SimpleVector<T>&& other):
+    value(other.value), size(other.Size()), capacity(other.Capacity())
+{
+    cout << "Move constructor" << endl;
+    other.value = nullptr;
+    other.size = 0;
+    other.capacity= 0;
+}
+//copy constructor
+template <typename T>
+SimpleVector<T>::SimpleVector(const SimpleVector<T>& other):
+    value(new T[other.Size()]), size(other.Size()), capacity(other.Capacity())
+{
+    cout << "Copy constructor" << endl;
+    copy(other.begin(), other.end(), begin());
+}
+
 
 template <typename T>
 SimpleVector<T>::~SimpleVector()
@@ -53,15 +79,14 @@ T& SimpleVector<T>::operator[](size_t index)
     {
         cout << "Exception happens: " << ex.what();
     }
-
 } 
 
 template <typename T>
-T* SimpleVector<T>::begin()
+T* SimpleVector<T>::begin() const
 { return value; }
 
 template <typename T>
-T* SimpleVector<T>::end()
+T* SimpleVector<T>::end() const
 { return value + size; }
 
 template <typename T>
@@ -124,13 +149,13 @@ void TestEqual_2()
 {
     const size_t SPECIAL_SIZE = 5;
     SimpleVector<float> sv(5);
-    sv[1] = 1.2;
-    sv[2] = 5.5;
-    sv[3] = 4.4;
-    sv[4] = 3.0;
-    sv[0] = 2.7;
+    sv[1] = 1.2f;
+    sv[2] = 5.5f;
+    sv[3] = 4.4f;
+    sv[4] = 3.0f;
+    sv[0] = 2.7f;
     //sv[5] = 3; 
-    float array[SPECIAL_SIZE] = { 2.7, 1.2, 5.5, 4.4, 3.0};
+    float array[SPECIAL_SIZE] = { 2.7f, 1.2f, 5.5f, 4.4f, 3.0f};
 
     for (size_t i = 0; i < SPECIAL_SIZE; ++i)
     {
@@ -142,11 +167,11 @@ void TestEqual_3()
 {
     const size_t SPECIAL_SIZE = 5;
     SimpleVector<float> sv(5);
-    sv[1] = 1.2;
-    sv[2] = 5.5;
-    sv[3] = 4.4;
-    sv[4] = 3.0;
-    sv[0] = 2.7;
+    sv[1] = 1.2f;
+    sv[2] = 5.5f;
+    sv[3] = 4.4f;
+    sv[4] = 3.0f;
+    sv[0] = 2.7f;
     //sv[5] = 3; 
     auto it_begin = sv.begin();
     auto it_end = sv.end();
@@ -193,5 +218,7 @@ int main()
    RUN_TEST(tr, TestEqual_2);
    RUN_TEST(tr, TestEqual_3);
    RUN_TEST(tr, TestEqual_4);
+
+    system("Pause");
     return 0;
 }
